aa3a82bfb9e4cbc023d7956022d7e237f8413914
sayeemjakhrof <47790968+sayeemjakhrof@users.noreply.github.com>
2023-08-04 23:23:43 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  const int a = 7;
+  const int b = 6;
+  //Addition
+  print(a + b);
+  //Subtraction
+  print(a - b);
+  //Multiplication
+  print(a * b);
+  //Division
+  print(a / b);
+  //Modulus
+  print(a % b);
+}
